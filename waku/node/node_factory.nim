549b3140d54e6c171858bc082cb63b94f6ca2175
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2024-02-16 15:19:58 +0200

    start implementing node factory

---
+++ waku/node/node_factory.nim
@@ -0,0 +1,72 @@
+import
+  chronicles
+
+## Peer persistence
+
+const PeerPersistenceDbUrl = "peers.db"
+proc setupPeerStorage(): Result[Option[WakuPeerStorage], string] =
+  let db = ? SqliteDatabase.new(PeerPersistenceDbUrl)
+
+  ? peer_store_sqlite_migrations.migrate(db)
+
+  let res = WakuPeerStorage.new(db)
+  if res.isErr():
+    return err("failed to init peer store" & res.error)
+
+  ok(some(res.value))
+
+## Retrieve dynamic bootstrap nodes (DNS discovery)
+
+proc retrieveDynamicBootstrapNodes*(dnsDiscovery: bool,
+                                    dnsDiscoveryUrl: string,
+                                    dnsDiscoveryNameServers: seq[IpAddress]):
+                                    Result[seq[RemotePeerInfo], string] =
+
+  if dnsDiscovery and dnsDiscoveryUrl != "":
+    # DNS discovery
+    debug "Discovering nodes using Waku DNS discovery", url=dnsDiscoveryUrl
+
+    var nameServers: seq[TransportAddress]
+    for ip in dnsDiscoveryNameServers:
+      nameServers.add(initTAddress(ip, Port(53))) # Assume all servers use port 53
+
+    let dnsResolver = DnsResolver.new(nameServers)
+
+    proc resolver(domain: string): Future[string] {.async, gcsafe.} =
+      trace "resolving", domain=domain
+      let resolved = await dnsResolver.resolveTxt(domain)
+      return resolved[0] # Use only first answer
+
+    var wakuDnsDiscovery = WakuDnsDiscovery.init(dnsDiscoveryUrl, resolver)
+    if wakuDnsDiscovery.isOk():
+      return wakuDnsDiscovery.get().findPeers()
+        .mapErr(proc (e: cstring): string = $e)
+    else:
+      warn "Failed to init Waku DNS discovery"
+
+  debug "No method for retrieving dynamic bootstrap nodes specified."
+  ok(newSeq[RemotePeerInfo]()) # Return an empty seq by default
+
+proc setupNode*(conf: WakuNodeConf): Result[WakuNode, string] =
+  var peerStoreOpt: Option[WakuPeerStorage]
+  
+  debug "1/7 Setting up storage"
+
+  ## Peer persistence
+  if conf.peerPersistence:
+    let peerStore = setupPeerStorage().valueOr:
+      error "1/7 Setting up storage failed", error = "failed to setup peer store " & error
+      return err("Setting up storage failed " & error)
+    peerStoreOpt = some(peerStore)
+
+  debug "2/7 Retrieve dynamic bootstrap nodes"
+
+  let dynamicBootstrapNodes = retrieveDynamicBootstrapNodes(conf.dnsDiscovery,
+                                                            conf.dnsDiscoveryUrl,
+                                                            conf.dnsDiscoveryNameServers).valueOr:
+    error "2/7 Retrieving dynamic bootstrap nodes failed", error = res3.error
+    return err("Retrieving bootstrap nodese failed " & error)
+  
+
+
+  
